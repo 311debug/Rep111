2385fe72cc814531ca1905bd6530930623765090
311debug <3loj311@gmail.com>
2026-04-16 09:25:23 +0500

    111

---
+++ MUD4/STM32CubeIDE/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C:/INSTALL/TouchGFXProjects/MUD4/TouchGFX/App/app_touchgfx.c \
+ C:/INSTALL/TouchGFXProjects/MUD4/TouchGFX/App/app_touchgfx.h
+C:/INSTALL/TouchGFXProjects/MUD4/TouchGFX/App/app_touchgfx.h:
---
+++ MUD4/STM32CubeIDE/Debug/Drivers/BSP/Components/ili9341.d
@@ -0,0 +1,6 @@
+Drivers/BSP/Components/ili9341.o: \
+ C:/INSTALL/TouchGFXProjects/MUD4/Drivers/BSP/Components/ili9341/ili9341.c \
+ C:/INSTALL/TouchGFXProjects/MUD4/Drivers/BSP/Components/ili9341/ili9341.h \
+ C:/INSTALL/TouchGFXProjects/MUD4/Drivers/BSP/Components/ili9341/../Common/lcd.h
+C:/INSTALL/TouchGFXProjects/MUD4/Drivers/BSP/Components/ili9341/ili9341.h:
+C:/INSTALL/TouchGFXProjects/MUD4/Drivers/BSP/Components/ili9341/../Common/lcd.h:
